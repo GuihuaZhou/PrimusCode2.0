bb0db6b151154e4ffc5654398529efdb1b69555d
GuihuaZhou <gulagulahuli@outlook.com>
2020-04-15 11:42:33 +0800

    解决问题，udp转发出错是因为master下发信息时没有对udp包进行转发范围判断

--- Primus/primus.cpp
+++ Primus/primus.cpp
@@ -2870,14 +2870,14 @@ Primus::SendToAllAffectedNodes(struct message tempMessage,int tempStartIndex,int
             numOfSentNodes++;
           }
           cout << "[ret:" << ret << "]." << endl;
-          // for (int j=0;j<MAX_FOWNODE_NUM;j++)
-          // {
-          //   nodeIndex=rand()%nodeSockNum;
-          //   if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
-          //   {
-          //     SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
-          //   }
-          // }
+          for (int j=0;j<MAX_FOWNODE_NUM;j++)
+          {
+            nodeIndex=rand()%nodeSockNum;
+            if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
+            {
+              SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
+            }
+          }
         }
         else if (nodeSockTable[i].nodeIdent.level==2 
           && nodeSockTable[i].nodeIdent.position%m_LeafNodes==tempIndex)// leafnode，只有相对位置相同才发送
@@ -2889,14 +2889,14 @@ Primus::SendToAllAffectedNodes(struct message tempMessage,int tempStartIndex,int
             numOfSentNodes++;
           }
           cout << "[ret:" << ret << "]." << endl;
-          // for (int j=0;j<MAX_FOWNODE_NUM;j++)
-          // {
-          //   nodeIndex=rand()%nodeSockNum;
-          //   if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
-          //   {
-          //     SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
-          //   }
-          // }
+          for (int j=0;j<MAX_FOWNODE_NUM;j++)
+          {
+            nodeIndex=rand()%nodeSockNum;
+            if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
+            {
+              SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
+            }
+          }
         }
         else if (nodeSockTable[i].nodeIdent.level==3
           && ((SameNode(tempMessage.linkInfo.identA,nodeSockTable[i].nodeIdent)) || (SameNode(tempMessage.linkInfo.identB,nodeSockTable[i].nodeIdent))))
@@ -2908,14 +2908,14 @@ Primus::SendToAllAffectedNodes(struct message tempMessage,int tempStartIndex,int
             numOfSentNodes++;
           }
           cout << "[ret:" << ret << "]." << endl;
-          // for (int j=0;j<MAX_FOWNODE_NUM;j++)
-          // {
-          //   nodeIndex=rand()%nodeSockNum;
-          //   if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
-          //   {
-          //     SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
-          //   }
-          // }
+          for (int j=0;j<MAX_FOWNODE_NUM;j++)
+          {
+            nodeIndex=rand()%nodeSockNum;
+            if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
+            {
+              SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
+            }
+          }
         }
         else if (nodeSockTable[i].nodeIdent.level==3)
         {
@@ -2930,14 +2930,14 @@ Primus::SendToAllAffectedNodes(struct message tempMessage,int tempStartIndex,int
                 numOfSentNodes++;
               }
               cout << "[ret:" << ret << "]." << endl;
-              // for (int j=0;j<MAX_FOWNODE_NUM;j++)
-              // {
-              //   nodeIndex=rand()%nodeSockNum;
-              //   if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
-              //   {
-              //     SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
-              //   }
-              // }
+              for (int j=0;j<MAX_FOWNODE_NUM;j++)
+              {
+                nodeIndex=rand()%nodeSockNum;
+                if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
+                {
+                  SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
+                }
+              }
             }
           }
           else if (tempMessage.linkInfo.identA.level==1 || tempMessage.linkInfo.identB.level==1)//leafnode--tornode
@@ -2951,14 +2951,14 @@ Primus::SendToAllAffectedNodes(struct message tempMessage,int tempStartIndex,int
                 numOfSentNodes++;
               }
               cout << "[ret:" << ret << "]." << endl;
-              // for (int j=0;j<MAX_FOWNODE_NUM;j++)
-              // {
-              //   nodeIndex=rand()%nodeSockNum;
-              //   if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
-              //   {
-              //     SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
-              //   }
-              // }
+              for (int j=0;j<MAX_FOWNODE_NUM;j++)
+              {
+                nodeIndex=rand()%nodeSockNum;
+                if (nodeSockTable[nodeIndex].nodeIdent.level!=0 && (i!=nodeIndex) && nodeSockTable[nodeIndex].nodeSock!=-1)
+                {
+                  SendMessageByTCP(nodeSockTable[nodeIndex].nodeSock,tempMessage);// 模拟udp，转发node会将其转为udp发送，
+                }
+              }
             }
           }
         }
